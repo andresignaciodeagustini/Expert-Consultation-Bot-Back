ada368db503227425bcbce3a86a6fcf30edc690a
andresignaciodeagustini <andresignaciodeagustini@gmail.com>
2025-02-19 13:58:24 -0300

    chore: update configuration files and force new deployment

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -8,7 +8,7 @@ SECTORS = ['automotive', 'technology', 'healthcare', 'finance', 'retail']
 
 VERIFIED_CLIENT_RESPONSES = [
     "Thank you. What sector or industry are you looking to explore?",
-    "Great, I've verified your account. Which industry sector would you like to explore?",
+    "Great!, I've verified your account. Which industry sector would you like to explore?",
     "Perfect, I've confirmed your client status. What industry should we focus on?",
     "Excellent! To help you find the right experts, which sector are you interested in?"
 ]
