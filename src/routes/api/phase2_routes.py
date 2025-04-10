3a97299d7df1d1c3faf0aa3e80377f2397b595f4
andresignaciodeagustini <andresignaciodeagustini@gmail.com>
2025-04-10 13:47:07 +0200

    Cambios

--- src/routes/api/phase2_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify
-from ...handlers.phase2_handlers import sector_handler, company_handler
-
-phase2_routes = Blueprint('phase2', __name__)
-
-@phase2_routes.route('/sector-experience', methods=['POST'])
-def handle_sector_experience():
-    return sector_handler.handle_sector_experience(request)
